eee24e3e1e907cd1c3c96bc3d1779e509513fd65
Zakhar Polikarpov <zpolikarpov@veka.de>
2024-07-11 12:59:48 +0200

    Basic Cards - Phone

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react"
+import icon_ellipsis from "./assets/images/icon-ellipsis.svg"
+
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("/data.json")
+      .then((oResponse) => {
+        return oResponse.json();
+      })
+      .then((arData) => {
+        setData(arData)
+      });
+  }, []);
+
+
 	return (
 		<>
-			<h1>Hello World!</h1>
+			<main className="container" data-type="full-bleed">
+        <div className="card"></div>
+        { 
+          data.map((oCategory, index) => {
+            return <div key={index} className="card" data-category={oCategory.title}>
+              <div className="card__inner">
+                <div className="card__header">
+                  <h3 className="card__heading">{oCategory.title}</h3>
+                  <button className="button" data-type="transparent"><img src={icon_ellipsis} alt="Details Button" /></button>
+                </div>
+                <div className="card__body">
+                  <h1 className="heading-1">{oCategory.timeframes.daily.current}hrs</h1>
+                  <p>Last Week - {oCategory.timeframes.daily.previous}hrs</p>
+                </div>
+              </div>
+            </div>
+          })
+        }
+      </main>
 		</>
   )
 }
